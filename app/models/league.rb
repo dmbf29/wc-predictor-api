c94fc13b5146fcdc70df668988d15ef40e5000ef
Douglas Berkley <douglasmberkley@gmail.com>
2018-05-23 18:29:49 +0900

    validations done

--- app/models/league.rb
+++ app/models/league.rb
@@ -1,4 +1,7 @@
 class League < ApplicationRecord
   has_many :memberships
   has_many :users, through: :memberships
+
+  validates :name, presence: true
+  validates :password, presence: true
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -2,4 +2,7 @@ class Team < ApplicationRecord
   belongs_to :group
   has_many :unions
   has_many :groups, through: :unions
+
+  validates :name, presence: true
+  validates :abbrev, presence: true
 end
