da226f5e19a824fd57ee9dd28d5fc5427fee92c1
Douglas Berkley <douglasmberkley@gmail.com>
2018-05-25 13:34:23 +0900

    added active model serializer gem

--- app/controllers/api/v1/matches_controller.rb
+++ app/controllers/api/v1/matches_controller.rb
@@ -1,7 +1,7 @@
 module Api::V1
   class MatchesController < ApplicationController
     def index
-      @matches = Match.all
+      @matches = Match.all.includes(:round)
       render json: @matches
     end
   end
