1239c0f4290ea765344b6cd312415f3665c2c2a2
Douglas Berkley <douglasmberkley@gmail.com>
2018-05-27 21:49:45 +0900

    ordered the matches by kickoff

--- app/controllers/api/v1/matches_controller.rb
+++ app/controllers/api/v1/matches_controller.rb
@@ -1,7 +1,7 @@
 module Api::V1
   class MatchesController < ApplicationController
     def index
-      matches = Match.all
+      matches = Match.all.order(kickoff_time: :asc)
       render json: matches, each_serializer: Api::V1::MatchSerializer
     end
   end
