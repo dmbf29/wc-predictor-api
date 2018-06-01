3724ba5bc9c4247df27988a9791371a8e8b8c4e5
Douglas Berkley <douglasmberkley@gmail.com>
2018-06-01 11:43:51 +0900

    grouped matches by group

--- app/controllers/api/v1/matches_controller.rb
+++ app/controllers/api/v1/matches_controller.rb
@@ -3,7 +3,7 @@ module Api::V1
     before_action :authenticate_user
 
     def index
-      matches = Match.all.order(kickoff_time: :asc)
+      matches = Match.all.order(kickoff_time: :asc).group_by(&:group_id)
       render json: matches, each_serializer: Api::V1::MatchSerializer, scope: current_user
     end
   end
