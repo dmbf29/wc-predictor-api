72a434f66f34cd04f2d89b8bbd81e240e9a16c9c
Douglas Berkley <douglasmberkley@gmail.com>
2018-06-07 16:57:17 +0900

    added user show

--- app/controllers/api/v1/matches_controller.rb
+++ app/controllers/api/v1/matches_controller.rb
@@ -6,5 +6,11 @@ module Api::V1
       matches = Match.all.order(:kickoff_time, group_id: :asc)
       render json: matches, each_serializer: Api::V1::MatchSerializer, scope: current_user
     end
+
+    def show
+      user_provided = User.find(params[:id])
+      groups = Group.all.order(id: :asc)
+      render json: groups, each_serializer: Api::V1::GroupSerializer, scope: user_provided
+    end
   end
 end
