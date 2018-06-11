12c9ef572aaeb9a7c23fff0ff1ad401b21b0a1f6
Douglas Berkley <douglasmberkley@gmail.com>
2018-06-11 23:02:58 +0900

    updated associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  has_many :memberships
+  has_many :memberships, dependent: :destroy
   has_many :leagues, through: :memberships
-  has_many :predictions
+  has_many :predictions, dependent: :destroy
   validates :name, presence: true, length: { minimum: 2, maximum: 30 }
   validates :email, presence: true, uniqueness: true, length: { minimum: 2, maximum: 30 }
   validates :password, presence: true, length: { minimum: 2, maximum: 30 }
