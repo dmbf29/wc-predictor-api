776208891e7898b6e2410acc8a431381e64249b1
Douglas Berkley <douglasmberkley@gmail.com>
2018-06-11 15:45:15 +0900

    added mailer and gmail credentials

---
+++ test/mailers/previews/user_mailer_preview.rb
@@ -0,0 +1,9 @@
+# Preview all emails at http://localhost:3000/rails/mailers/user_mailer
+class UserMailerPreview < ActionMailer::Preview
+
+  # Preview this email at http://localhost:3000/rails/mailers/user_mailer/knockout_stage
+  def knockout_stage
+    UserMailer.knockout_stage
+  end
+
+end
