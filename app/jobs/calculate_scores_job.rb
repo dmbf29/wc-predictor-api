424c2413fcaced4a2c6f6d0ef5e5069ecb5b7b35
Douglas Berkley <douglasmberkley@gmail.com>
2018-06-14 15:44:39 +0900

    updated scores job

--- app/jobs/calculate_scores_job.rb
+++ app/jobs/calculate_scores_job.rb
@@ -13,49 +13,106 @@ class CalculateScoresJob < ApplicationJob
 
     puts 'Looking at fixtures...'
     html_doc.search('.fi-mu.fixture').each do |element|
-      time = element.search('.fi-mu__info').search('.fi-mu__info__datetime').text.strip.split('-')[0]
-      kickoff_time = element.search('.fi-s__scoreText').text.strip
-      venue = element.search('.fi__info__venue').text.strip
-      time += kickoff_time
-      group_name = element.search('.fi-mu__info').search('.fi__info__group').text.strip
+
+      puts 'Finding Team Names...'
       team_home_name = element.search('.fi-t.fi-i--4.home').search('.fi-t__nText').text.strip
       team_away_name = element.search('.fi-t.fi-i--4.away').search('.fi-t__nText').text.strip
-      if venue == 'Kaliningrad'
-        parsed_time = ActiveSupport::TimeZone['Europe/Kaliningrad'].parse(time).utc
-      elsif venue == 'Moscow' || venue == 'Kazan' || venue == 'Nizhny Novgorod' || venue == 'Rostov-On-Don' || venue == 'Saransk' || venue == 'Sochi' || venue == 'St. Petersburg' || venue == 'Volgograd'
-        parsed_time = ActiveSupport::TimeZone['Europe/Moscow'].parse(time).utc
-      elsif venue == 'Samara'
-        parsed_time = ActiveSupport::TimeZone['Europe/Samara'].parse(time).utc
-      elsif venue == 'Ekaterinburg'
-        parsed_time = ActiveSupport::TimeZone['Asia/Yekaterinburg'].parse(time).utc
-      else
-        raise
-      end
-      puts 'Groups..'
-      group = Group.find_by(name: group_name)
+      puts '*********'
+      puts team_home_name
+      puts team_away_name
+      puts '*********'
+
+      puts 'Finding Team Scores...'
+      team_home_score = "TODO"
+      team_away_score = "TODO"
+      puts '*********'
+      puts team_home_score
+      puts team_away_score
+      puts '*********'
+
+      puts "Looking for team instances"
       team_home = Team.find_by(name: team_home_name)
       team_away = Team.find_by(name: team_away_name)
+      puts '*********'
+      puts team_home.name
+      puts team_away.name
+      puts '*********'
 
-      next if team_home.blank? || team_away.nil?
+      next if team_home_score.blank? || team_away_score.blank?
 
-      puts 'Unions...'
-      # Union.where(
-      #   team: team_home,
-      #   group: group
-      # ).first_or_create
+      puts 'Looking for the match...'
+      match = Match.where(
+        team_home: team_home,
+        team_away: team_away,
+        finished: false
+      ).first
+      if match.nil?
+        puts '*********'
+        puts '** COULDNT FIND MATCH **'
+        puts '*********'
+      end
+      next if match.nil?
 
-      puts 'Matches...'
-      # match = Match.where(
-      #   group: group,
-      #   round: round_one,
-      #   team_home_id: team_home.id,
-      #   team_away_id: team_away.id
-      # ).first_or_create
-      # puts match.team_home.name + ' vs. ' + match.team_away.name
-      # puts 'Old time: ' + match.kickoff_time
-      # match.kickoff_time = parsed_time
-      # puts 'New time: ' + match.kickoff_time
-      # match.save
+      if team_home_score > team_away_score
+        match.finished = true
+        match.draw = false
+        match.winner_id = team_home.id
+        match.team_home_score = team_home_score
+        match.team_away_score = team_away_score
+        if match.save
+          match.predections.where(winner_id: team_home.id).each do |predection|
+            predection.correct = true
+            user = predection.user
+            user.score += 3
+            user.score_group += 3
+            predection.save
+            user.save
+          end
+          match.predections.where.not(winner_id: team_home.id).each do |predection|
+            predection.correct = false
+            predection.save
+          end
+        end
+      elsif team_home_score < team_away_score
+        match.finished = true
+        match.draw = false
+        match.winner_id = team_away.id
+        match.team_home_score = team_home_score
+        match.team_away_score = team_away_score
+        if match.save
+          match.predections.where(winner_id: team_away.id).each do |predection|
+            predection.correct = true
+            user = predection.user
+            user.score += 3
+            user.score_group += 3
+            predection.save
+            user.save
+          end
+          match.predections.where.not(winner_id: team_away.id).each do |predection|
+            predection.correct = false
+            predection.save
+          end
+        end
+      elsif team_home_score == team_away_score
+        match.finished = true
+        match.draw = true
+        match.team_home_score = team_home_score
+        match.team_away_score = team_away_score
+        if match.save
+          match.predections.where(draw: true).each do |predection|
+            predection.correct = true
+            user = predection.user
+            user.score += 3
+            user.score_group += 3
+            predection.save
+            user.save
+          end
+          match.predections.where.not(draw: true).each do |predection|
+            predection.correct = false
+            predection.save
+          end
+        end
+      end
     end
   end
 end
