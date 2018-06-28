8b04e231fe2dd0f617f9175e4d56e73a1b737b50
Douglas Berkley <douglasmberkley@gmail.com>
2018-06-28 20:03:32 +0100

    Update bracket sides

--- app/controllers/deciders_controller.rb
+++ app/controllers/deciders_controller.rb
@@ -2,10 +2,16 @@ class DecidersController < ApplicationController
   def england
     winner = Match.find_by(team_home: Team.find_by_name("Group H Winner"))
     england = Team.find_by_name('England')
+    next_match = Match.find_by(team_away: Team.find_by_name("Winner of 54"))
+    winner.group= Group.find_by_name("Left Sixteen")
+    winner.next_match = next_match
     winner.team_home = england
     winner.save
     runner = Match.find_by(team_away: Team.find_by_name('Group H Runner-up'))
     belgium = Team.find_by_name('Belgium')
+    next_match = Match.find_by(team_away: Team.find_by_name("Winner of 56"))
+    runner.group= Group.find_by_name("Right Sixteen")
+    runner.next_match = next_match
     runner.team_away = belgium
     runner.save
   end
@@ -13,10 +19,16 @@ class DecidersController < ApplicationController
   def belgium
     winner = Match.find_by(team_home: Team.find_by_name('Group H Winner'))
     belgium = Team.find_by_name('Belgium')
+    next_match = Match.find_by(team_away: Team.find_by_name("Winner of 54"))
+    winner.group= Group.find_by_name("Left Sixteen")
+    winner.next_match = next_match
     winner.team_home = belgium
     winner.save
     runner = Match.find_by(team_away: Team.find_by_name('Group H Runner-up'))
     england = Team.find_by_name('England')
+    next_match = Match.find_by(team_away: Team.find_by_name("Winner of 56"))
+    winner.group= Group.find_by_name("Right Sixteen")
+    runner.next_match = next_match
     runner.team_away = england
     runner.save
   end
